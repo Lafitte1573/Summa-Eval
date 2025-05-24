568d3aaf5d00b5d2429ac2cf644252a91c7dd20b
edger <18810317098@163.com>
2025-05-24 16:30:26 +0800

    update files

--- print_to_table.py
+++ print_to_table.py
@@ -9,8 +9,11 @@ if __name__ == '__main__':
         'lcsts.jsonl'
     ]
     for file in eval_datasets:
+        print("#### " + file.split('.')[0].upper())
+        print('| Model | ROUGE-1 | ROUGE-2 | ROUGE-L | ROUGE-W |')
+        print(r'| ---  | --- | --- | --- | --- |')
         lines = [json.loads(line) for line in open(path.join('results', file)).readlines()]
         for line in lines:
             # print(line)
-            print(' & '.join([f'{100*float(v):.2f}' if k != 'model' else v for k, v in line.items()]) + r' \\')
+            print(' | ' + ' | '.join([f'{100*float(v):.2f}' if k != 'model' else v for k, v in line.items()]) + ' | ')
         print()
